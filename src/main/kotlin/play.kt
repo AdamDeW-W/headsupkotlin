782dd0258e5515f2bf90c86a8d5dc9d638355677
AdamD <lowkeycoat1@gmail.com>
2023-12-13 09:17:54 -0600

    trying to push

---
+++ src/main/kotlin/play.kt
@@ -0,0 +1,13 @@
+fun main() {
+
+    var x = 99
+    println(" $x bottles of beer on the wall")
+    while (x > 0) {
+        println(" $x Bottles of beer.")
+        x = x - 1
+
+        println("take one down and pass it around.")
+    }
+}
+
+
